f00eace27c4fb06ed3d734a57b907dc2296652b0
creatub <blatherr@gmail.com>
2024-05-02 21:00:13 +0900

    update gpt

--- backend/src/main/java/com/team2/resumeeditorproject/resume/repository/ResumeEditRepository.java
+++
@@ -1,7 +0,0 @@
-package com.team2.resumeeditorproject.resume.repository;
-
-import com.team2.resumeeditorproject.resume.domain.ResumeEdit;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResumeEditRepository extends JpaRepository<ResumeEdit, Long> {
-}
--- backend/src/main/java/com/team2/resumeeditorproject/user/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.team2.resumeeditorproject.user.repository;
-
-import com.team2.resumeeditorproject.user.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
-
-

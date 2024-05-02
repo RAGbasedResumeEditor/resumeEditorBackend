cfa51284884c84549094119964abe6bd3f3b11b8
creatub <blatherr@gmail.com>
2024-05-02 21:17:10 +0900

    update gpt

---
+++ backend/src/main/java/com/team2/resumeeditorproject/gpt/controller/ChatGPTController.java
@@ -0,0 +1,101 @@
+package com.team2.resumeeditorproject.gpt.controller;
+
+import com.team2.resumeeditorproject.gpt.dto.ChatCompletionDto;
+import com.team2.resumeeditorproject.gpt.dto.CompletionDto;
+import com.team2.resumeeditorproject.gpt.service.ChatGPTService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * ChatGPT API
+ *
+ * @author : 김상휘
+ * @fileName : ChatGPTController
+ * @since : 04/25/24
+ */
+@Slf4j
+@RestController
+@RequestMapping(value = "/api/v1/chatGpt")
+public class ChatGPTController {
+
+    private final ChatGPTService chatGPTService;
+
+    public ChatGPTController(ChatGPTService chatGPTService) {
+        this.chatGPTService = chatGPTService;
+    }
+
+    /**
+     * [API] ChatGPT 모델 리스트를 조회합니다.
+     */
+    @GetMapping("/modelList")
+    public ResponseEntity<List<Map<String, Object>>> selectModelList() {
+        List<Map<String, Object>> result = chatGPTService.modelList();
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    /**
+     * [API] ChatGPT 유효한 모델인지 조회합니다.
+     *
+     * @param modelName
+     * @return
+     */
+    @GetMapping("/model")
+    public ResponseEntity<Map<String, Object>> isValidModel(@RequestParam(name = "modelName") String modelName) {
+        Map<String, Object> result = chatGPTService.isValidModel(modelName);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    /**
+     * [API] Legacy ChatGPT 프롬프트 명령을 수행합니다. : gpt-3.5-turbo-instruct, babbage-002, davinci-002
+     *
+     * @param completionDto {}
+     * @return ResponseEntity<Map < String, Object>>
+     */
+    @PostMapping("/legacyPrompt")
+    public ResponseEntity<Map<String, Object>> selectLegacyPrompt(@RequestBody CompletionDto completionDto) {
+        log.debug("param :: " + completionDto.toString());
+        Map<String, Object> result = chatGPTService.legacyPrompt(completionDto);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    /**
+     * [API] 최신 ChatGPT 프롬프트 명령어를 수행합니다. : gpt-4, gpt-4 turbo, gpt-3.5-turbo
+     *
+     * @param chatCompletionDto
+     * @return
+     */
+    @PostMapping("/prompt")
+    public ResponseEntity<Map<String, Object>> selectPrompt(@RequestBody ChatCompletionDto chatCompletionDto) {
+        log.debug("param :: " + chatCompletionDto.toString());
+        Map<String, Object> result = chatGPTService.prompt(chatCompletionDto);
+//        return new ResponseEntity<>(result, HttpStatus.OK);
+        // "choices" 키에 해당하는 값 가져오기
+        if (result.containsKey("choices")) {
+            List<Map<String, Object>> choices = (List<Map<String, Object>>) result.get("choices");
+            if (!choices.isEmpty()) {
+                Map<String, Object> firstChoice = choices.get(0);
+                if (firstChoice.containsKey("message")) {
+                    Map<String, Object> message = (Map<String, Object>) firstChoice.get("message");
+                    if (message.containsKey("content")) {
+                        String content = (String) message.get("content");
+                        Map<String, Object> response = new HashMap<>();
+                        response.put("content", content);
+                        response.put("status","Success");
+                        return ResponseEntity.ok(response); // 성공 상태 반환
+                    }
+                }
+            }
+        }
+
+        // 실패 상태 반환
+        Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("status", "Fail");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+    }
+}

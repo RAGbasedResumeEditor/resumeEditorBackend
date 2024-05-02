2e9f1ad1d6dea3d31f4b516fdc44fef84a374f97
YeonGyeong <iamyeongyeong@gmail.com>
2024-05-02 17:05:30 +0900

    dd

--- backend/src/main/java/com/team2/resumeeditorproject/user/Jwt/JWTUtil.java
+++ backend/src/main/java/com/team2/resumeeditorproject/user/Jwt/JWTUtil.java
@@ -30,13 +30,21 @@ public class JWTUtil { // ver 0.12.3
     public String getCategory(String token){
         return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("category", String.class);
     }
-    public String createJwt(String username, String role, Long expiredMs){
+    // 토큰을 생성할 메서드
+    public String createJwt(String category,String username, String role, Long expiredMs){
+
         return Jwts.builder()
+                //claim : JWT에 저장되는 정보
+                .claim("category", category) //access, refresh 판단하여 refresh토큰을 가지고 access에 접근하면 사용할 수 없도록 설정
                 .claim("username", username)
-                .claim("role",role)
+                .claim("role", role)
+                // 토큰 발행시간
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis()+expiredMs))
+                // 토큰 소멸시간(만료시간)
+                .expiration(new Date(System.currentTimeMillis() + expiredMs))
+                // 최종 암호화
                 .signWith(secretKey)
+                // 토큰 생성
                 .compact();
     }
 }
--- backend/src/main/java/com/team2/resumeeditorproject/user/Jwt/LoginFilter.java
+++ backend/src/main/java/com/team2/resumeeditorproject/user/Jwt/LoginFilter.java
@@ -3,8 +3,10 @@ package com.team2.resumeeditorproject.user.Jwt;
 import com.team2.resumeeditorproject.user.dto.CustomUserDetails;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -37,7 +39,7 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
     }
     @Override // 인증 성공 시
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication){
-        System.out.println("Success");
+        /*System.out.println("Success");
 
         CustomUserDetails customUserDetails=(CustomUserDetails) authentication.getPrincipal();
         String username=customUserDetails.getUsername();
@@ -51,7 +53,36 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
         String token=jwtUtil.createJwt(username, role, 60*60*10L); // jwt 생성
 
         response.addHeader("Authorization", "Bearer " + token); // 인증방식 + 토큰
+         */
+        //Access/Refresh 토큰 발급-----------------------------------
+        //유저 정보(username, role) 꺼내오기
+        String username = authentication.getName();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority auth = iterator.next();
+        String role = auth.getAuthority();
+
+        //토큰 생성(JWTUtil 에서 발급한 응답 값 호출)
+        String access = jwtUtil.createJwt("access", username, role, 600000L); //생명주기 10분
+        String refresh = jwtUtil.createJwt("refresh", username, role, 86400000L); //생명주기 24시간
+
+        //응답 설정
+        response.setHeader("access", access);
+        response.addCookie(createCookie("refresh", refresh));
+        response.setStatus(HttpStatus.OK.value()); //200
     }
+
+    private Cookie createCookie(String key, String value) {
+        Cookie cookie = new Cookie(key, value);
+        cookie.setMaxAge(24*60*60); //생명주기
+        //cookie.setSecure(true); //HttpS?통신을 진행할 경우 이 값을 넣어줌
+        //cookie.setPath("/");
+        cookie.setHttpOnly(true); //클라이언트단에서 자바스크립트로 해당 쿠키가 접근하지 못하도록 필수적으로 막아 두어야함
+
+        return cookie;
+    }
+
 
     @Override // 인증 실패 시
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {

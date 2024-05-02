b7acc2d09348bf3bc3bae67a3ad4b9ebb8045c35
creatub <blatherr@gmail.com>
2024-05-02 21:35:39 +0900

    test complete

---
+++ backend/src/main/java/com/team2/resumeeditorproject/config/ChatGPTConfig.java
@@ -0,0 +1,35 @@
+package com.team2.resumeeditorproject.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * ChatGPT에서 사용하는 환경 구성
+ *
+ * @author : 김상휘
+ * @fileName : RestTemplate
+ * @since : 04/25/24
+ */
+@Configuration
+public class ChatGPTConfig {
+
+    @Value("${openai.secret-key}")
+    private String secretKey;
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public HttpHeaders httpHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(secretKey);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return headers;
+    }
+}

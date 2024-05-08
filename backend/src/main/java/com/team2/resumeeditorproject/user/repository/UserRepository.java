c37286e2cfb485e7a0b4174ffd41236ab6398082
YeonGyeong <iamyeongyeong@gmail.com>
2024-05-08 11:30:28 +0900

    admin

--- backend/src/main/java/com/team2/resumeeditorproject/user/repository/UserRepository.java
+++ backend/src/main/java/com/team2/resumeeditorproject/user/repository/UserRepository.java
@@ -9,11 +9,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Boolean existsByEmail(String email);
     Boolean existsByUsername(String username);
-<<<<<<< HEAD
-    User findByUsername(String username); // username으로 DB 회원 정보 조회하는 메서드
-=======
     User findByUsername(String username);
->>>>>>> 8b97b5ac7d556187da118a8fe88d7f4326108f33
 }
 
 
--- backend/src/main/java/com/team2/resumeeditorproject/user/service/UserService.java
+++ backend/src/main/java/com/team2/resumeeditorproject/user/service/UserService.java
@@ -1,17 +1,90 @@
 package com.team2.resumeeditorproject.user.service;
 
 import com.team2.resumeeditorproject.user.dto.UserDTO;
-import org.springframework.validation.Errors;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 public interface UserService {
     Long signup(UserDTO userDto); // 회원가입
     Boolean checkEmailDuplicate(String email);
-<<<<<<< HEAD
-    Boolean checkUsernameDuplicate(String nickname);
-=======
     Boolean checkUsernameDuplicate(String username);
->>>>>>> 8b97b5ac7d556187da118a8fe88d7f4326108f33
+
+    @Controller
+    @AllArgsConstructor
+    class UserController extends HttpServlet {
+
+        private final UserService userService;
+
+        @PostMapping(value="/signup")
+        public ResponseEntity<Map<String,Object>> signup(@RequestBody UserDTO userDto, HttpServletResponse res) throws IOException {
+
+            Map<String,Object> response=new HashMap<>();
+            Map<String,Object> errorResponse=new HashMap<>();
+
+            try{ //회원가입 성공 시
+                Long result= userService.signup(userDto);
+                response.put("status","Success"); //1. 상태(실패)
+                response.put("time", new Date()); //2. 시간
+                response.put("response","회원가입 성공"); //3. 메시지 내용(성공)
+                return ResponseEntity.ok(response);
+            }catch(Exception e){ // 회원가입 실패 시
+                errorResponse.put("status","Fail"); //1. 상태(실패)
+                errorResponse.put("time",new Date()); //2. 시간
+                errorResponse.put("response", "서버 오류입니다."); //3. 메시지 내용(에러)
+
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+            }
+        }//signup()
+
+        @PostMapping("/signup/exists/email")
+        public ResponseEntity<Map<String,Object>> checkEmailDuplicate(@RequestParam("email") String email){
+            Map<String,Object> response=new HashMap<>();
+            Map<String,Object> errorResponse=new HashMap<>();
+            try{
+                boolean result= userService.checkEmailDuplicate(email);
+                //System.out.println("email check result (중복 시 true): "+result);
+                response.put("status","Success");
+                response.put("result",result+"");
+                response.put("time", new Date());
+                response.put("response", "이메일 중복 여부 확인 성공");
+                return ResponseEntity.ok(response);
+            }catch(Exception e){
+                errorResponse.put("status","Fail");
+                errorResponse.put("time",new Date());
+                errorResponse.put("response", "서버 오류입니다.");
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+            }
+        }
+
+        @PostMapping("/signup/exists/username")
+        public ResponseEntity<Map<String,Object>> checkUsernameDuplicate(@RequestParam("username") String username){
+                Map<String,Object> response=new HashMap<>();
+                Map<String,Object> errorResponse=new HashMap<>();
+                try{
+                    boolean result=userService.checkUsernameDuplicate(username); // 중복 시 true
+                    response.put("status","Success");
+                    response.put("result",result+"");
+                    response.put("time", new Date());
+                    response.put("response", "Username 중복 여부 확인 성공");
+                    return ResponseEntity.ok(response);
+                }catch(Exception e){
+                    errorResponse.put("status","Fail");
+                    errorResponse.put("time",new Date());
+                    errorResponse.put("response", "서버 오류입니다.");
+                    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+                }
+        }
+    }
 }

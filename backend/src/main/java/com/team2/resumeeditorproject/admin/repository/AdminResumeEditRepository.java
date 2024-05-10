c14a6f07508a19792b7e086e7119c576c803cf90
YeonGyeong <iamyeongyeong@gmail.com>
2024-05-10 14:28:18 +0900

    admin repository updates

--- backend/src/main/java/com/team2/resumeeditorproject/admin/repository/AdminResumeEditRepository.java
+++ backend/src/main/java/com/team2/resumeeditorproject/admin/repository/AdminResumeEditRepository.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 @Repository
 public interface AdminResumeEditRepository extends JpaRepository<ResumeEdit, Long> {
-    // 여기에 ResumeEdit 엔티티에 특화된 메서드를 추가할 수 있습니다.
-    //ResumeEdit
     List<ResumeEdit> findByCompany(String company);
+    List<ResumeEdit> findByOccupation(String occupation);
 }

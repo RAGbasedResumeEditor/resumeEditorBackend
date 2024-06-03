413b884ceb834087cc33dab5cbfc132f39b0fdd9
ibnuena <ibnuena@naver.com>
2024-06-03 15:31:57 +0900

    review upload

---
+++ backend/src/main/java/com/team2/resumeeditorproject/review/service/ReviewService.java
@@ -0,0 +1,8 @@
+package com.team2.resumeeditorproject.review.service;
+
+import com.team2.resumeeditorproject.review.dto.ReviewDTO;
+
+public interface ReviewService {
+
+    void insertReview(ReviewDTO reviewDTO);
+}

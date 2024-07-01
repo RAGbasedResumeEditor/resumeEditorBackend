f730c2934355326c9e86ea66f1b7bc1b2f6cbe9e
ibnuena <ibnuena@naver.com>
2024-07-01 18:36:12 +0900

    admin refactoring fetch, history update

--- src/main/java/com/team2/resumeeditorproject/admin/controller/AdminController.java
+++ src/main/java/com/team2/resumeeditorproject/admin/controller/AdminController.java
@@ -23,9 +23,11 @@ import static com.team2.resumeeditorproject.admin.service.ResponseHandler.create
 import static com.team2.resumeeditorproject.admin.service.ResponseHandler.createBadRequestResponse;
 
 @Controller
-@RequestMapping("/admin/stat")
+@RequestMapping("/admin/statistics")
 @RequiredArgsConstructor
 public class AdminController {
+
+    private static final String Invalid_Request_Error_Message = "잘못된 요청입니다.";
 
     private final AdminService adminService;
     private final HistoryService historyService;
@@ -33,35 +35,36 @@ public class AdminController {
 
     /* 유저 정보에 관한 통계 */
     @GetMapping("/user")
-    public ResponseEntity<Map<String,Object>> getUserCnt(@RequestParam(name="group", required=false) String group,
+    public ResponseEntity<Map<String,Object>> getUserStatistics(@RequestParam(name="group", required=false) String group,
                                                          @RequestParam(name="occupation", required = false) String occupation,
                                                          @RequestParam(name="wish", required = false) String wish) {
-            Function<String, ResponseEntity<Map<String, Object>>> action = switch (group) {
-                case "count" -> (g) -> createOkResponse(adminService.userCnt());
-                case "gender" -> (g) -> createOkResponse(adminService.genderCnt());
-                case "age" -> (g) -> createOkResponse(adminService.ageCnt());
-                case "status" -> (g) -> createOkResponse(adminService.statusCnt());
-                case "mode" -> (g) -> createOkResponse(adminService.modeCnt());
-                case "occupation" -> (g) -> createOkResponse(adminService.occupCnt(occupation));
-                case "wish" -> (g) -> createOkResponse(adminService.wishCnt(wish));
-                case "pro" -> (g) -> createOkResponse(historyService.getProUserCnt());
-                case "visitTotal" -> (g) -> createOkResponse(historyService.getTotalTraffic());
-                case "visitToday" -> (g) -> createOkResponse(historyService.getTrafficForCurrentDate());
-                default -> (g) ->  createBadRequestResponse("잘못된 요청입니다.");
-            };
-            return action.apply(group);
+        Function<String, ResponseEntity<Map<String, Object>>> action = switch (group) {
+            case "count" -> (g) -> createOkResponse(adminService.userCnt());
+            case "gender" -> (g) -> createOkResponse(adminService.genderCnt());
+            case "age" -> (g) -> createOkResponse(adminService.ageCnt());
+            case "status" -> (g) -> createOkResponse(adminService.statusCnt());
+            case "mode" -> (g) -> createOkResponse(adminService.modeCnt());
+            case "occupation" -> (g) -> createOkResponse(adminService.occupCnt(occupation));
+            case "wish" -> (g) -> createOkResponse(adminService.wishCnt(wish));
+            case "pro" -> (g) -> createOkResponse(historyService.getProUserCnt());
+            case "visitTotal" -> (g) -> createOkResponse(historyService.getTotalTraffic());
+            case "visitToday" -> (g) -> createOkResponse(historyService.getTrafficForCurrentDate());
+            default -> (g) ->  createBadRequestResponse(Invalid_Request_Error_Message); // - 에러메세지 상수로 변경
+        };
+        return action.apply(group);
     }
 
     // 일별 접속자 집계
     @GetMapping("/user/access")
-    public ResponseEntity<Map<String,Object>> getAccessCount(
+    public ResponseEntity<Map<String,Object>> getDailyAccessStatistics(
             @RequestParam(value = "startDate", required = false) @DateTimeFormat(pattern = "yy-MM-dd") Optional<LocalDate> startDate,
             @RequestParam(value = "endDate", required = false) @DateTimeFormat(pattern = "yy-MM-dd") Optional<LocalDate> endDate) {
         try {
             Map<String, Object> result = new LinkedHashMap<>();
+            LocalDate currentDate = LocalDate.now();
 
-            LocalDate start = startDate.orElse(LocalDate.now().minusDays(6)); // startDate가 주어지지 않으면 현재 날짜로부터 7일 전으로 설정
-            LocalDate end = endDate.orElse(LocalDate.now()); // endDate가 주어지지 않으면 현재 날짜로 설정
+            LocalDate start = startDate.orElse(currentDate.minusDays(6)); // startDate가 주어지지 않으면 현재 날짜로부터 7일 전으로 설정
+            LocalDate end = endDate.orElse(currentDate); // endDate가 주어지지 않으면 현재 날짜로 설정
 
             Map<LocalDate, Integer> trafficData = trafficService.getTrafficData(start, end);
 
@@ -75,7 +78,7 @@ public class AdminController {
 
     // 월별 접속자 집계
     @GetMapping("/user/access/monthly")
-    public ResponseEntity<Map<String, Object>> getMonthlyAccessCount(
+    public ResponseEntity<Map<String, Object>> getMonthlyAccessStatistics(
             @RequestParam(value = "startDate", required = false) @DateTimeFormat(pattern = "yyyy-MM") Optional<YearMonth> startDate) {
         try {
             Map<String, Object> result = new LinkedHashMap<>();
@@ -98,7 +101,7 @@ public class AdminController {
 
     // 일별 회원가입 집계
     @GetMapping("/user/signup")
-    public ResponseEntity<Map<String,Object>> getSignupStatistics(
+    public ResponseEntity<Map<String,Object>> getDailySignupStatistics(
             @RequestParam(value = "startDate", required = false) @DateTimeFormat(pattern = "yy-MM-dd") Optional<LocalDate> startDate,
             @RequestParam(value = "endDate", required = false) @DateTimeFormat(pattern = "yy-MM-dd") Optional<LocalDate> endDate) {
         try {
@@ -145,45 +148,45 @@ public class AdminController {
 
     /* 자소서 목록에 관한 통계 */
     @GetMapping("/board")
-    public ResponseEntity<Map<String,Object>> getResumeStatByCompany(@RequestParam(name="group") String group,
+    public ResponseEntity<Map<String,Object>> getResumeBoardStatistics(@RequestParam(name="group") String group,
                                                                      @RequestParam(name="company", required = false) String company,
                                                                      @RequestParam(name="occupation", required=false) String occupation) {
-            Function<String, ResponseEntity<Map<String, Object>>> action = switch (group) {
-                case "company" -> (g) -> createOkResponse(adminService.CompResumeCnt(company));
-                case "occupation" -> (g) -> createOkResponse(adminService.OccupResumeCnt(occupation));
-                default -> (g) ->  createBadRequestResponse("잘못된 요청입니다.");
-            };
-            return action.apply(group);
+        Function<String, ResponseEntity<Map<String, Object>>> action = switch (group) {
+            case "company" -> (g) -> createOkResponse(adminService.CompResumeCnt(company));
+            case "occupation" -> (g) -> createOkResponse(adminService.OccupResumeCnt(occupation));
+            default -> (g) ->  createBadRequestResponse(Invalid_Request_Error_Message);
+        };
+        return action.apply(group);
     }
 
     /* 자소서 첨삭 이용에 관한 통계 */
-    @GetMapping("/resumeEdit")
-    public ResponseEntity<Map<String, Object>> getResumeEditCountByStatus(@RequestParam(name="group", required = false) String group,
+    @GetMapping("/resume-edit")
+    public ResponseEntity<Map<String, Object>> getResumeEditStatistics(@RequestParam(name="group", required = false) String group,
                                                                           @RequestParam(name="company", required = false) String company,
                                                                           @RequestParam(name="occupation", required = false) String occupation
                                                                           ) {
-            Function<String, ResponseEntity<Map<String, Object>>> action = switch (group) {
-                case "status" -> (g) -> createOkResponse(adminService.resumeEditCntByStatus());
-                case "company" -> (g) -> createOkResponse(adminService.resumeEditCntByComp(company));
-                case "occupation" -> (g) -> createOkResponse(adminService.resumeEditCntByOccup(occupation));
-                case "age" -> (g) -> createOkResponse(adminService.resumeEditCntByAge());
-                case "mode" -> (g) -> createOkResponse(adminService.resumeEditCntByMode());
-                case "monthly" -> (g) -> createOkResponse(adminService.resumeCntByMonth()); // 채용시즌(월별)
-                case "weekly" -> (g) -> createOkResponse(adminService.resumeCntByWeekly()); // 채용시즌(주별)
-                case "daily" -> (g) -> createOkResponse(adminService.resumeCntByDaily()); // 채용시즌(일별)
-                default -> (g) -> createBadRequestResponse("잘못된 요청입니다");
-            };
-            return action.apply(group);
+        Function<String, ResponseEntity<Map<String, Object>>> action = switch (group) {
+            case "status" -> (g) -> createOkResponse(adminService.resumeEditCntByStatus());
+            case "company" -> (g) -> createOkResponse(adminService.resumeEditCntByComp(company));
+            case "occupation" -> (g) -> createOkResponse(adminService.resumeEditCntByOccup(occupation));
+            case "age" -> (g) -> createOkResponse(adminService.resumeEditCntByAge());
+            case "mode" -> (g) -> createOkResponse(adminService.resumeEditCntByMode());
+            case "monthly" -> (g) -> createOkResponse(adminService.resumeCntByMonth()); // 채용시즌(월별)
+            case "weekly" -> (g) -> createOkResponse(adminService.resumeCntByWeekly()); // 채용시즌(주별)
+            case "daily" -> (g) -> createOkResponse(adminService.resumeCntByDaily()); // 채용시즌(일별)
+            default -> (g) -> createBadRequestResponse(Invalid_Request_Error_Message);
+        };
+        return action.apply(group);
     }
 
     // 관리자 - 통계 페이지 내에서 각 영역 top 5를 출력한다.
     @GetMapping("/rank/occupation")
-    public ResponseEntity<Map<String, Object>> getOccupRank() {
+    public ResponseEntity<Map<String, Object>> getOccupationRank() {
         return createOkResponse(adminService.rankOccup());
     }
 
     @GetMapping("/rank/company")
-    public ResponseEntity<Map<String, Object>> getCompRank() {
+    public ResponseEntity<Map<String, Object>> getCompanyRank() {
         return createOkResponse(adminService.rankComp());
     }
 
@@ -194,18 +197,18 @@ public class AdminController {
 
     // 자소서 통계
     @GetMapping("/resume/count")
-    public ResponseEntity<Map<String,Object>> getResumeStat(@RequestParam(name="group", required = false) String group) {
+    public ResponseEntity<Map<String,Object>> getResumeStatistics(@RequestParam(name="group", required = false) String group) {
         Function<String, ResponseEntity<Map<String, Object>>> action = switch (group) {
             case "editTotal" -> (g) -> createOkResponse(historyService.getTotalEdit());
             case "editToday" -> (g) -> createOkResponse(historyService.getRNumForCurrentDate());
             case "boardToday" -> (g) -> createOkResponse(historyService.getTotalBoardCnt());
-            default -> (g) -> createBadRequestResponse("잘못된 요청입니다");
+            default -> (g) -> createBadRequestResponse(Invalid_Request_Error_Message);
         };
         return action.apply(group);
     }
 
     @GetMapping("/resume/monthly")
-    public ResponseEntity<Map<String,Object>> getEditStatByMonthly() {
+    public ResponseEntity<Map<String,Object>> getMonthlyResumeEditStatistics() {
         try {
             Map<String, Object> result = new LinkedHashMap<>();
 
@@ -218,7 +221,7 @@ public class AdminController {
     }
 
     @GetMapping("/resume/weekly")
-    public ResponseEntity<Map<String,Object>> getEditStatByWeekly(@RequestParam(name="month", required = false) String month) {
+    public ResponseEntity<Map<String,Object>> getWeeklyResumeEditStatistics(@RequestParam(name="month", required = false) String month) {
         try {
             return createOkResponse(historyService.getEditByWeekly(month));
         } catch (Exception e) {
@@ -227,7 +230,7 @@ public class AdminController {
     }
 
     @GetMapping("/resume/daily")
-    public ResponseEntity<Map<String,Object>> getEditStatByDaily(@RequestParam(value = "startDate", required = false) String startDate,
+    public ResponseEntity<Map<String,Object>> getDailyResumeEditStatistics(@RequestParam(value = "startDate", required = false) String startDate,
                                                                  @RequestParam(value = "endDate", required = false) String endDate) {
         try {
             Map<String, Object> result = new LinkedHashMap<>();

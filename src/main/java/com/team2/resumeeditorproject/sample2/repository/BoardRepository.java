fa5515edea841d6b8dbba1a53b55d7a4d9be5e75
Baek-SeungJae <tmdwo897@naver.com>
2024-07-04 02:57:38 +0900

    remove sample2 package

--- src/main/java/com/team2/resumeeditorproject/sample2/repository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.team2.resumeeditorproject.sample2.repository;
-
-import com.team2.resumeeditorproject.sample2.domain.Board;
-import org.springframework.data.repository.CrudRepository;
-public interface BoardRepository extends CrudRepository<Board, Long>{
-
-}

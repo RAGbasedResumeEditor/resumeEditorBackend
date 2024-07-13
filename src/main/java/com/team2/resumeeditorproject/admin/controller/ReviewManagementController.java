513c6757fa16d99fd32d8a98367dc987a04bf236
ibnuena <ibnuena@naver.com>
2024-07-13 23:07:03 +0900

    review management controller, review management service/impl 수정

--- src/main/java/com/team2/resumeeditorproject/admin/controller/ReviewManagementController.java
+++ src/main/java/com/team2/resumeeditorproject/admin/controller/ReviewManagementController.java
@@ -1,13 +1,15 @@
 package com.team2.resumeeditorproject.admin.controller;
 
-import com.team2.resumeeditorproject.admin.dto.ReviewDTO;
 import com.team2.resumeeditorproject.admin.dto.response.ReviewDisplayResultResponse;
 import com.team2.resumeeditorproject.admin.dto.response.ReviewListResponse;
 import com.team2.resumeeditorproject.admin.service.ReviewManagementService;
+import com.team2.resumeeditorproject.common.util.PageUtil;
 import com.team2.resumeeditorproject.exception.NotFoundException;
 import com.team2.resumeeditorproject.review.domain.Review;
+import com.team2.resumeeditorproject.review.dto.ReviewDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -35,95 +37,36 @@ public class ReviewManagementController {
 
         int size = SIZE_OF_PAGE;
 
-        if (pageNo < 0) {
-            pageNo = 0;
-        }
-
-        Page<Review> reviewList = reviewService.getPagedReviews(pageNo);
-
-        List<ReviewDTO> reviewDTOList = new ArrayList<>();
-        for (Review review : reviewList) {
-            ReviewDTO reviewDTO = new ReviewDTO();
-            reviewDTO.setRv_num(review.getRvNum());
-            reviewDTO.setU_num(review.getUNum());
-            reviewDTO.setContent(review.getContent());
-            reviewDTO.setRating(review.getRating());
-            reviewDTO.setMode(review.getMode());
-            reviewDTO.setW_date(review.getW_date());
-            reviewDTO.setDisplay(review.getDisplay());
-            reviewDTOList.add(reviewDTO);
-        }
-
-        if (reviewDTOList.isEmpty()) {
-            throw new NotFoundException("후기가 존재하지 않습니다.");
-        }
+        Page<ReviewDTO> reviewPage = reviewService.getPagedReviews(pageNo, size);
 
         return ResponseEntity.ok()
                 .body(ReviewListResponse.builder()
-                .totalPage(reviewList.getTotalPages())
-                .reviewDTOList(reviewDTOList)
+                        .totalPage(reviewPage.getTotalPages())
+                        .reviewDTOList(reviewPage.getContent())
                         .build());
     }
 
     @GetMapping("/list/display-review")
     public ResponseEntity<ReviewListResponse> getShowReviews(@RequestParam("pageNo") int pageNo) {
-        if (pageNo < 0) {
-            pageNo = 0;
-        }
+        int size = SIZE_OF_PAGE;
 
-        Page<Review> reviewList = reviewService.getDisplayReviews(pageNo);
-
-//        List<ReviewDTO> reviewDTOList = new ArrayList<>();
-//        for (Review review : reviewList) {
-//            ReviewDTO reviewDTO = new ReviewDTO();
-//            reviewDTO.setRv_num(review.getRvNum());
-//            reviewDTO.setU_num(review.getUNum());
-//            reviewDTO.setContent(review.getContent());
-//            reviewDTO.setRating(review.getRating());
-//            reviewDTO.setMode(review.getMode());
-//            reviewDTO.setW_date(review.getW_date());
-//            reviewDTO.setDisplay(review.getDisplay());
-//            reviewDTOList.add(reviewDTO);
-//        }
-        List<ReviewDTO> reviewDTOList = reviewList.stream()
-                .map(review -> new ReviewDTO(
-                        review.getRvNum(),
-                        review.getUNum(),
-                        review.getContent(),
-                        review.getRating(),
-                        review.getMode(),
-                        review.getW_date(),
-                        review.getDisplay()
-                ))
-                .collect(Collectors.toList());
-
-        if (reviewDTOList.isEmpty()) {
-            throw new NotFoundException("후기가 존재하지 않습니다.");
-        }
+        Page<ReviewDTO> reviewPage = reviewService.getDisplayReviews(pageNo, size);
 
         return ResponseEntity.ok()
                 .body(ReviewListResponse.builder()
-                        .totalPage(reviewList.getTotalPages())
-                        .reviewDTOList(reviewDTOList)
+                        .totalPage(reviewPage.getTotalPages())
+                        .reviewDTOList(reviewPage.getContent())
                         .build());
     }
 
     @PostMapping("/display")
     public ResponseEntity<ReviewDisplayResultResponse> selectDisplayReview(@RequestParam("reviewNo") Long reviewNo) {
-        try {
-            return ResponseEntity.ok()
-                    .body(ReviewDisplayResultResponse.builder()
-                            .response(reviewService.selectReview(reviewNo))
-                            .status("Success")
-                            .time(new Date())
-                            .build());
-        } catch (Exception e) {
-            return ResponseEntity.badRequest()
-                    .body(ReviewDisplayResultResponse.builder()
-                            .response("Failed to selected")
-                            .status("Fail")
-                            .time(new Date())
-                            .build());
-        }
+        return ResponseEntity.ok()
+                .body(ReviewDisplayResultResponse.builder()
+                        .response(reviewService.selectReview(reviewNo))
+                        .status("Success")
+                        .time(new Date())
+                        .build());
+
     }
 }
--- src/main/java/com/team2/resumeeditorproject/admin/dto/response/ReviewListResponse.java
+++ src/main/java/com/team2/resumeeditorproject/admin/dto/response/ReviewListResponse.java
@@ -1,7 +1,7 @@
 package com.team2.resumeeditorproject.admin.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.team2.resumeeditorproject.admin.dto.ReviewDTO;
+import com.team2.resumeeditorproject.review.dto.ReviewDTO;
 import lombok.Builder;
 import lombok.Getter;
 import java.util.List;

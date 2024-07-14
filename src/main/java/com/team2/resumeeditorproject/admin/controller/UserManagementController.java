84d168978c2fe6e606f00a5384944cb13f6a86fd
ibnuena <ibnuena@naver.com>
2024-07-14 16:28:22 +0900

    searchUserRequest @ModelAttribute로 변경

--- src/main/java/com/team2/resumeeditorproject/admin/controller/UserManagementController.java
+++ src/main/java/com/team2/resumeeditorproject/admin/controller/UserManagementController.java
@@ -12,8 +12,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -45,7 +45,7 @@ public class UserManagementController {
 
     /* 회원 검색 */
     @GetMapping("/list/search")
-    public ResponseEntity<UserListResponse> searchUsers(@RequestBody SearchUserRequest searchUserRequest) {
+    public ResponseEntity<UserListResponse> searchUsers(@ModelAttribute SearchUserRequest searchUserRequest) {
 
         Page<UserDTO> userPage = userManagementService.searchUsersByGroupAndKeyword(searchUserRequest, SIZE_OF_PAGE);
 
